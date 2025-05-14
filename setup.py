1ba6b9e15cbbde161491ed93663bffb12c5b5e3e
YaqiXia <94524206+YaqiXia@users.noreply.github.com>
2025-05-14 17:18:07 +0800

    Update setup.py
    fix typo

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     setuptools.setup(
         name="voltrix",
         version="1.0.0",
-        packeges=find_packages(),
+        packages=find_packages(),
         cmdclass={},
     )
